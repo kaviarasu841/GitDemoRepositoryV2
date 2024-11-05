e5c2e96e1cfb080ab0d0d06de126f26a762b7f7d
Join2Kavi <join2kavi@gmail.com>
2024-11-05 11:33:27 +0530

    Update Test1.java

--- src/Test1.java
+++ src/Test1.java
@@ -2,9 +2,9 @@
 public class Test1 {
 	
 	public static void main(String args[]) {
-		String hungry = "yes";
+		String hungry = "Yes";
 		
-		if(hungry == "yes")
+		if(hungry == "Yes")
 			System.out.println("eat something....");
 		else {
 			System.out.println("Do work...");
